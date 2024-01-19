ec8a966341bc7709098a6ba3e1ca60ef2194208c
ZIYAD MABROUK <mabrouk.ziyad@inemail.ine.inpt.ma>
2024-01-19 11:43:59 +0100

    v2.0

--- hello.java
+++ hello.java
@@ -5,6 +5,9 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour, aujourd'hui on va travailler avec GIT");
+		
+		// New line
+		System.out.println("Ziyad says hello from Agadir!");
 
 	}
 
