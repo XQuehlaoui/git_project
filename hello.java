06540bbd15dcf134784551c4f5043a23c9ecc00d
mohamed quehlaoui <quehlaoui02@gmail.com>
2024-01-19 10:44:10 +0100

    helloworld

---
+++ hello.java
@@ -0,0 +1,11 @@
+package java_github;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour, aujourd'hui on va travailler avec GIT");
+
+	}
+
+}
